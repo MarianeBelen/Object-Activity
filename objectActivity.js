ccbcbb07e242f25920837d67591261726ed43fe1
MarianeBelen <marianebelen15@gmail.com>
2025-03-28 18:04:53 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,20 @@
+/*
+BELEN, Mariane I.
+BSCS 1B
+MIDTERM EXAM - DSA
+objectActivity.js
+*/
+
+let car = {
+    type: "SUVs",
+    model: "Honda",
+    color: "Black"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car)
